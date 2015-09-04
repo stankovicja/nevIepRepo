2796a68f6fbc91d95f17fdd625581a9dd7b602a3
stankovicja <stankovic1107@gmail.com>
2015-09-04 17:27:08 +0200

    init commit

---
+++ nevIepProject/nevIepProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nevIepProject.Startup))]
+namespace nevIepProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
